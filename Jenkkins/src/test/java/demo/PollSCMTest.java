5e0c003df4bc182d96e3830b9c1b6deb980b00da
dell <dell@DESKTOP-M1U6SU2>
2025-02-06 13:54:32 +0530

    third

---
+++ Jenkkins/src/test/java/demo/PollSCMTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class PollSCMTest {
+@Test
+public void scm() {
+	Reporter.log("poll scm class added",true);
+}
+}
