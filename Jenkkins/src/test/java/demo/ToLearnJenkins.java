a5a2ce9ec1c8fb5058ae51efd619c415bca133c7
dell <dell@DESKTOP-M1U6SU2>
2025-02-06 13:04:43 +0530

    jenkins

---
+++ Jenkkins/src/test/java/demo/ToLearnJenkins.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnJenkins {
+@Test
+public void jenkins() {
+	Reporter.log("jenkins class added",true);
+}
+}
