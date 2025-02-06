86c0001dfda0a06c447b7580cec9fa9fafd7293c
dell <dell@DESKTOP-M1U6SU2>
2025-02-06 13:25:37 +0530

    second

--- Jenkkins/src/test/java/demo/ToLearnJenkinsTest.java
+++ Jenkkins/src/test/java/demo/ToLearnJenkinsTest.java
@@ -3,9 +3,10 @@ package demo;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class ToLearnJenkins {
+public class ToLearnJenkinsTest {
 @Test
 public void jenkins() {
 	Reporter.log("jenkins class added",true);
+	Reporter.log("success message",true);
 }
 }
